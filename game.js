4a339076bc36a3ac0566a9dc4cb26be662b65f31
PatrickB001 <patrickborgella1@gmail.com>
2022-04-06 00:54:48 -0500

    Initial commit adds game data, ui and controls. Few UI/Control  bugs that need to be fixed

---
+++ game.js
@@ -0,0 +1,87 @@
+const gameData = {
+  pattern: [],
+  userClickedPattern: [],
+  buttonColors: ["red", "blue", "green", "yellow"],
+  button: $("div[type=button]"),
+  started: false,
+  level: 0,
+};
+
+const gameUI = {
+  animatePress(currentColor) {
+    $(`#${currentColor}`).addClass("pressed");
+    setTimeout(() => {
+      $(`#${currentColor}`).removeClass("pressed");
+    }, 100);
+  },
+  loadAudio() {
+    let audio = new Audio("./sounds/green.mp3");
+  },
+  playSound(name) {
+    let audio = new Audio(`./sounds/${name}.mp3`);
+    audio.play();
+  },
+};
+
+const gameControls = {
+  initGame() {
+    gameUI.loadAudio();
+    this._handleUserClicks();
+    $(document).keypress(() => {
+      if (gameData.started === false) {
+        this._nextSequence();
+        gameData.started = true;
+      }
+    });
+  },
+  _startOver() {
+    gameData.level = 0;
+    gameData.pattern = [];
+    gameData.started = false;
+  },
+  _nextSequence() {
+    gameData.userClickedPattern = [];
+    gameData.level++;
+    $("#level-title").text(`Level ${gameData.level}`);
+    let randomizer = Math.floor(Math.random() * 4);
+    let randomColor = gameData.buttonColors[randomizer];
+    gameData.pattern.push(randomColor);
+    $(`#${randomColor}`).fadeOut(150).fadeIn(150);
+
+    gameUI.playSound(randomColor);
+  },
+  _checkAnswer(currentLevel) {
+    if (
+      gameData.userClickedPattern[currentLevel] ===
+      gameData.pattern[currentLevel]
+    ) {
+      if (gameData.userClickedPattern.length === gameData.pattern.length) {
+        setTimeout(() => {
+          this._nextSequence();
+        }, 875);
+      }
+    } else {
+      $("body").addClass("game-over");
+      $("h1").text("Game Over, Press Any Key to Restart");
+      setTimeout(() => {
+        $("body").removeClass("game-over");
+      }, 300);
+      let wrongAudio = new Audio("./sounds/wrong.mp3");
+      wrongAudio.play();
+
+      this._startOver();
+    }
+  },
+  _handleUserClicks() {
+    $(gameData.button).click((e) => {
+      let userChosenColor = e.target.id;
+      gameData.userClickedPattern.push(userChosenColor);
+
+      gameUI.playSound(userChosenColor);
+      gameUI.animatePress(userChosenColor);
+      this._checkAnswer(gameData.userClickedPattern.length - 1);
+    });
+  },
+};
+
+gameControls.initGame();
